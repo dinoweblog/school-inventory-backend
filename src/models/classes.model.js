2dbc4700a26f08c5c657081cce636c97330b0c39
Dinesh Sharma <93374756+dinoweblog@users.noreply.github.com>
2022-04-17 20:34:38 +0530

    update

--- src/models/classes.model.js
+++ src/models/classes.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const classSchema = new mongoose.Schema(
   {
     grade: { type: String, required: true },
-    section: { type: Number, required: true },
+    section: { type: String, required: true },
     subject: [{ type: String, required: true }],
   },
   {
